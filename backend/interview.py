0da21dafa479c0d7e508626b4e1ed88eaf2e3d13
MrWei282 <z5363380@ad.unsw.edu.au>
2025-10-06 20:44:53 +1100

    basic user and interview server routes and some skeleton backend functions

---
+++ backend/interview.py
@@ -0,0 +1,8 @@
+def interview_start(token):
+    print("some questions to choose")
+
+def interview_text_answer(token):
+    print("answer interview")
+
+def interview_feedback(token, interview_question, interview_answer):
+    print("get interview feedback")
