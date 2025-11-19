4d234729bf90810e89730daacb2e78700c93ff55
MrWei282 <z5363380@ad.unsw.edu.au>
2025-11-19 16:59:16 +1100

    new deploy branch with features from main

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="Interview API", version="1.0.0")
 # --- CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://capstone-project-25t3-9900-f14b-cake.onrender.com",  # Production frontend
+        "http://localhost:3000",                                      # Local development
+        "http://172.19.0.2:3000",                                     # Docker local
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
